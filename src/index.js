8f409090816f48a42f4a260bc20f24c748efaec1
cvjude <jjchinosoviolet@gmail.com>
2019-08-26 17:23:28 +0100

    feat(Homepage): Design home page
    Create the navbar component
    Create the Homepage
    Write unit tests
    Add react router to move between pages
    
    [Delivers #168109547]

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './pages';
 import { Provider } from 'react-redux';
 import { store } from './createStore';
+import './style.css';
 
 ReactDOM.render(
   <Provider store={store}>
---
+++ src/pages/Homepage/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './style.css';
+
+const index = () => {
+  return (
+    <React.Fragment>
+      <section
+        className="home-bg"
+        style={{ height: '100vh' }}
+        id="home"
+        data-test="homeComponent"
+      >
+        <div className="container">
+          <div className="text-center">
+            <div className="pro_conditions">
+              <ul>
+                <li>Professional</li>
+                <li>Reliable</li>
+                <li>24 hours</li>
+              </ul>
+            </div>
+            <h2 className="header_title">
+              At banka we Love Making Things Amazing And Simple
+            </h2>
+            <p className="header_subtitle">
+              Experience the ease of using our services, with off the chart
+              professionalism and a whooping 24 hour service. In for a ride? get
+              started now.
+            </p>
+          </div>
+        </div>
+      </section>
+    </React.Fragment>
+  );
+};
+
+export default index;
